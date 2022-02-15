5ccc10f238265311ba587486b5dcb33a0ee51545
Kelly Young <kelpopko@gmail.com>
2022-02-15 14:56:42 -0600

    Add intro

--- authenticator.rb
+++ authenticator.rb
@@ -1,1 +1,23 @@
-// Udemy Project, #30
+users = [
+  { username: "kelly", password: "password1" },
+  { username: "otto", password: "password2" },
+  { username: "archer", password: "password3" },
+  { username: "masha", password: "password4" },
+  { username: "chester", "password": "password5"}
+]
+
+puts "Why hello, there. Welcome to the authenticator"
+25.times { print "-" }
+puts
+puts "You'll need to enter a username and password."
+puts "If the password is correct, you'll get back the user object. (What does that even mean)"
+
+puts "Username: "
+username = gets.chomp
+puts "Password: "
+password = gets.chomp
+
+puts "Your username is #{username} and your password is #{password}"
+
+
+
