574756ceae9dcbf2c4c2962ee8d19127bbc39269
Kelly Young <kelpopko@gmail.com>
2022-02-15 15:58:43 -0600

    Add first working version

--- authenticator.rb
+++ authenticator.rb
@@ -6,18 +6,38 @@ users = [
   { username: "chester", "password": "password5"}
 ]
 
-puts "Why hello, there. Welcome to the authenticator"
+
+puts "Welcome to the authenticator"
 25.times { print "-" }
 puts
-puts "You'll need to enter a username and password."
-puts "If the password is correct, you'll get back the user object. (What does that even mean)"
+puts "This program will take input from the user and compare passwor"
+puts "If the password is correct, you'll get back the user object."
 
-puts "Username: "
-username = gets.chomp
-puts "Password: "
-password = gets.chomp
+
+
+attempts = 1
+while attempts < 4
+  print "Username: "
+  username = gets.chomp.downcase
+  print "Password: "
+  password = gets.chomp
+
+  # loop through each el of array and compare
+  users.each do |user|
+    if user[:username] == username && user[:password] == password
+    puts user
+    break
+    else 
+      puts "Credentials were not correct"
+    end
+  end
+
+
+  puts "Press n to quit or any other key to continue: "
+  input = gets.chomp.downcase
+  break if input == 'n'
+  attempts += 1 
+end
 
 puts "Your username is #{username} and your password is #{password}"
-
-
 
